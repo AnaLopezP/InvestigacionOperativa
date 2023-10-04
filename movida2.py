fda27afb8dbcc2f7fe99a3831a1c102b4f971ad6
Ana López <anamuyamiga@gmail.com>
2023-10-04 18:50:21 +0200

    cambio nombre

--- movida2.py
+++
@@ -1,8 +0,0 @@
-import sympy as sym
-from sympy import symbols
-from sympy.plotting import plot
-f1 = "5+0*x"
-f2 = "8-2*x"
-f3 = "(34 - 4*x)/5"
-x = symbols('x')
-plot(f1, f2, f3, (x, 0, 5)) #no funca
