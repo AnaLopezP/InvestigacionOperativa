49ee69c3dfde1873a447d2abe6bc1495760c3f4a
Ana López <anamuyamiga@gmail.com>
2023-10-04 19:49:02 +0200

    xd

--- ejercicio2v3.py
+++ ejercicio2v3.py
@@ -7,4 +7,4 @@ f2 = "1+x" #x1 -x2 <= 1
 f3 = "4" # x2 <= 4
 x = symbols('x')
 plot(f1, f2, f3, (x, -1, 15))
-# Sol: múltiple
+# Sol: múltiple, ya que las horas de estudio pueden ir de 1 a 10 (eje x), y las horas de diversión se mantienen en 0 
--- ejercicio2v4.py
+++ ejercicio2v4.py
@@ -1,9 +1,10 @@
+#Aquí queremos divertirnos lo máximo posible, sin excluir estudiar
 import sympy as sym
 from sympy import symbols
 from sympy.plotting import plot
-f1 = "10-x" #<=
-f2 = "1+x" #<=
-#y=4
-f3 = "4" #<=
+f1 = "10-x" #x1 + x2 <= 10
+f2 = "1+x" #x1 -x2 <= 1
+f3 = "4" # x2 <= 4
 x = symbols('x')
 plot(f1, f2, f3, (x, -1, 15))
+#La solución es la recta acotada desde x1 = 3 a x1 = 6 y x2 = 4, ya que 4 son las horas máximas de diversión y el resto de horas para estudiar.
