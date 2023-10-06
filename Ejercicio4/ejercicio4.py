ab4147d415c93ddddd75807fcde35f7fa21fe285
Ana López <anamuyamiga@gmail.com>
2023-10-06 18:55:37 +0200

    datos del problema

---
+++ Ejercicio4/ejercicio4.py
@@ -0,0 +1,12 @@
+#Recursos: plástico 1200kg, horas 40h
+#Requerimientos: producción total <= 800, x1 - x2 <= 450
+#x1 --> space rays: 2kg, 3min
+#x2 --> Zappers: 1kg, 4min
+
+
+#max Z = 8x1 + 5x2
+#Plástico: 2x1 + x2 <= 1200
+#Horas: 3x1 + 4x2 <= 2400
+#x1 + x2 <= 800
+#x1 - x2 <= 450
+
