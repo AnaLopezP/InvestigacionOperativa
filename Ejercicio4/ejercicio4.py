45c2c1fb38ceb734353f7c884b19cc9594cd3816
Ana López <anamuyamiga@gmail.com>
2023-10-06 19:26:52 +0200

    bb

--- Ejercicio4/ejercicio4.py
+++ Ejercicio4/ejercicio4.py
@@ -24,7 +24,7 @@ Z3 = "(1000-8*x)/5"
 x = symbols('x')
 plot(f1, f2, f3, f4, Z1, (x, 0, 800))
 
-#¿se puede hacer mejor?
+#b) ¿se puede hacer mejor?
 '''Si, ya que hay otros puntos que equilibran más la producción de los juguetes. En la gráfica se ve que la función Z (morado) corta con el área óptima en el punto (450, 260). Z sigue valiendo 4900, pero la producción de juguetes está más equilibrada.'''
 
 #e) ¿x1 = 100; x2 = 150 es una solución factible? ¿Por qué?
