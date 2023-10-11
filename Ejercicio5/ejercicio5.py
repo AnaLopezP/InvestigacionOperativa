228feb653e9e4357db0371d96264f533302837c3
Ana López <anamuyamiga@gmail.com>
2023-10-11 20:03:50 +0200

    jhjjh

--- Ejercicio5/ejercicio5.py
+++ Ejercicio5/ejercicio5.py
@@ -1,2 +1,8 @@
 #mira si el profe hace algo lo pongo aqui pero vaya que tampoco estoy mucho por la labor
+#mahou roja --> x1
+#mahou verde --> x2
+#Max Z = 30x1 + 50x2
+#Embotellado: 2x1+x2 <= 230 (para 1000litros)
+#Distribución: x1+2x2 <= 250
+#x2 <= 120 (para 1000litros)
 
